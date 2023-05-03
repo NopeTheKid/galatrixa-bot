16a7a8e3ccfea4acc574d6545389ea8e32f7f34f
Nopearino <ajmc.p@hotmail.com>
2023-05-03 16:24:54 +0100

    * Fix being able do use music buttons without being in the voice channel

--- src/buttons/resume&pause.js
+++ src/buttons/resume&pause.js
@@ -1,5 +1,7 @@
 module.exports = async ({  inter, queue }) => {
     if (!queue) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
+    if (!inter.member.voice.channel) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | You are not in a Voice Channel`)], ephemeral: true, })
+    if (inter.guild.members.me.voice.channel && inter.member.voice.channel.id !== inter.guild.members.me.voice.channel.id) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | You are not in the same Voice Channel`)], ephemeral: true, })
 
     const success = queue.node.pause();
     
--- src/buttons/volumeup.js
+++ src/buttons/volumeup.js
@@ -1,6 +1,8 @@
 const maxVol = client.config.opt.maxVol;
 module.exports = async ({  inter, queue }) => { 
     if (!queue || !queue.node.isPlaying()) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
+    if (!inter.member.voice.channel) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | You are not in a Voice Channel`)], ephemeral: true, })
+    if (inter.guild.members.me.voice.channel && inter.member.voice.channel.id !== inter.guild.members.me.voice.channel.id) return inter.reply({ embeds: [ new EmbedBuilder().setColor('#ff0000').setDescription(`❌ | You are not in the same Voice Channel`)], ephemeral: true, })
 
     const vol = Math.floor(queue.volume + 5)
 
